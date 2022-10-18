46706b9edc97e0d51b6c403617d53c0b5b88040e
Marcel Görtz <mg@braincast.gmbh>
2022-10-18 12:14:01 +0200

    global handler

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import type Application from 'koa';
 import koaBody from 'koa-body';
 
-import { Config, mergeConfig, RequestData, Type } from '@ko-fi/types';
+import { kofiHandler } from '@ko-fi/handler';
+import { Config, mergeConfig } from '@ko-fi/types';
 
 import type Router from '@koa/router';
 
@@ -11,39 +12,6 @@ export const kofi = (router: Router, config?: Partial<Config<Application.Default
     router.post(conf.endpoint, koaBody(), async (ctx) => {
         const { data } = ctx.request.body as { data: string; };
 
-        try {
-            const parsed: RequestData = JSON.parse(data);
-
-            if (config.verificationToken && parsed.verification_token !== config.verificationToken) {
-                console.error('Ko-fi invalid verification token');
-                config.onError?.('Ko-fi invalid verification token', ctx);
-
-                return ctx.status = 401;
-            }
-
-            await conf.onData?.(parsed, ctx);
-
-            switch (parsed.type) {
-                case Type.Commission:
-                    await conf.onCommission?.(parsed, ctx);
-                    break;
-                case Type.Donation:
-                    await conf.onDonation?.(parsed, ctx);
-                    break;
-                case Type.ShopOrder:
-                    await conf.onShopOrder?.(parsed, ctx);
-                    break;
-                case Type.Subscription:
-                    await conf.onSubscription?.(parsed, ctx);
-                    break;
-            }
-        } catch (err) {
-            console.error('Ko-fi request error: ', err);
-            config.onError?.(err, ctx);
-
-            return ctx.status = 400;
-        }
-
-        ctx.status = 200;
+        ctx.status = await kofiHandler(data, conf, ctx);
     });
 };
